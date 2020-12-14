7d93c2e70b89b5b907b4c2ed72f8f847f1738864
Pineappologist <anastasia.romashka@gmail.com>
2020-12-14 21:22:45 +0300

    reads and outputs numbers

--- main.cpp
+++ main.cpp
@@ -29,5 +29,13 @@ int main() {
     int n(0);
     ifs >> n;
 
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++) {
+        ifs >> nums[i];
+    }
+
+    for (int i = 0; i < n; ++i) {
+        cout << nums[i] << endl;
+    }
 
 }
